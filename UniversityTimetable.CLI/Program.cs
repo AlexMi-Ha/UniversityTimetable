6aa352b0d94becf1ccd0b5c9382287be7034261c
Alex Hagl <alex@hagl.de>
2022-01-26 21:21:25 +0100

    Comments

--- UniversityTimetable.CLI/Program.cs
+++ UniversityTimetable.CLI/Program.cs
@@ -11,18 +11,24 @@ namespace UniversityTimetable.CLI {
     public class Program {
         public static void Main(string[] args) {
             while(true) {
+                // Listen to input
                 Console.Write("\n$ ");
                 string input = Console.ReadLine();
                 ProcessInput(input);
             }
         }
 
+        /// <summary>
+        /// Processes the text input and extracts commands + calls the necessary methods for the command
+        /// </summary>
+        /// <param name="input"></param>
         public static void ProcessInput(string input) {
             string[] inputTokenized = input.Split(" ");
             if (inputTokenized.Length == 0) {
                 Console.WriteLine("Invalid Input!");
                 return;
             }
+            // Read commands
             switch(inputTokenized[0].ToLower()) {
                 case "getweek":
                     ManageGetWeek(inputTokenized);
@@ -33,12 +39,18 @@ namespace UniversityTimetable.CLI {
             }
         }
 
-
+        /// <summary>
+        /// Manage the GetWeek command
+        /// Usage: getweek date timetableUrl
+        /// </summary>
+        /// <param name="input"></param>
         public static void ManageGetWeek(string[] input) {
             if(input.Length != 3) {
                 Console.WriteLine("Invalid number of Arguments! Usage: getweek <date> <timetableUrl>");
                 return;
             } 
+
+            // Verify date parameter
             DateTime date;
             try {
                 date = DateTime.Parse(input[1]);
@@ -47,13 +59,17 @@ namespace UniversityTimetable.CLI {
                 return;
             }
 
+            // Verify url parameter
             string url = input[2];
             string pattern = @"(?:http[s]?://)?(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+";
             if(!Regex.IsMatch(url, pattern)) {
                 Console.WriteLine("Invalid Url!");
                 return;
             }
+            // retrieve week list
             List<AppointmentDataModel>[] weekList = Scraper.GetWeek(date, url);
+
+            // Display it
             for(int day = 0; day < 5; day++) {
                 Console.WriteLine("\n-------------------------------------------\n");
                 Console.WriteLine(IndexToDay(day) + "\n");
@@ -68,6 +84,11 @@ namespace UniversityTimetable.CLI {
 
         #region Helper Methods
 
+        /// <summary>
+        /// Helper to convert an index to its corresponding day
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
         public static string IndexToDay(int i) {
             return i switch {
                 0 => "Monday",
--- UniversityTimetable.Core/Web/Scraper.cs
+++ UniversityTimetable.Core/Web/Scraper.cs
@@ -16,6 +16,7 @@ namespace UniversityTimetable.Core {
         }
 
         public static List<AppointmentDataModel>[] GetWeek(DateTime date) {
+            // if no timetable url is saved, get it from the FileManager
             if(String.IsNullOrWhiteSpace(timetableUrl)) {
                 FileManager man = new FileManager();
                 try {
@@ -25,8 +26,10 @@ namespace UniversityTimetable.Core {
                 }
             }
 
+            // assemble the full url
             string url = $"{timetableUrl}&day={date.Day}&month={date.Month}&year={date.Year}";
             
+            // Scrape the timetable
             HtmlWeb web = new HtmlWeb();
             HtmlDocument doc = web.Load(url);
             
@@ -38,22 +41,26 @@ namespace UniversityTimetable.Core {
             }
 
 
-            for(int i = 2; i < weekRows.Count; ++i) {
+            for(int i = 2; i < weekRows.Count; ++i) { // skip fist two header rows
                 var row = weekRows[i].SelectNodes("./td");
                 int day = 0;
                 for (int col = 1; col < row.Count; ++col) {
+                    // Loop until we find a back seperator element of a cell
                     if(!(row[col].GetClasses().Contains("week_separatorcell_black") || row[col].GetClasses().Contains("week_separatorcell"))) {
                         continue;
                     }
+                    // Check if the element infront of the back seperator is a week block (appointment)
                     if(row[col - 1].GetClasses().Contains("week_block")) {
 
+                        // Extract information from text using regex
                         string text = row[col - 1].SelectNodes("./a")[0].InnerText.Replace("&nbsp;", " ");
                         string pattern = @"^(\d{2}:\d{2}).{6}-(\d{2}:\d{2})(.+)";
                         var matches = Regex.Matches(text, pattern);
 
                         if (matches.Count > 0 && matches[0].Groups.Count > 1) {
                             DateTime start = DateTime.Parse(matches[0].Groups[1].Value);
-                            if (start.Hour < 8) continue;
+                            if (start.Hour < 8) continue; // Skip if it starts before 8;
+                            // TODO some appointments before 8 are information about the day -> maybe display it somehow?
 
                             DateTime end = DateTime.Parse(matches[0].Groups[2].Value);
 
@@ -62,6 +69,7 @@ namespace UniversityTimetable.Core {
                             });
                         }
                     }
+                    // After finding a back seperator element of a cell, skip to the next day
                     ++day;
                 }
             }
